f59143fe839d67216e6ddd728cffc256f0287082
CicakIndo <39509029+CicakIndo@users.noreply.github.com>
2019-02-17 15:04:32 +0700

    Create var.php

---
+++ var.php
@@ -0,0 +1,114 @@
+<?php
+
+${"GLOBALS"}["vrxougoybf"]		="hitam";
+${"GLOBALS"}["gysbjjimrf"]		="merah";
+${"GLOBALS"}["owmuxayhuu"]		="hijau";
+${"GLOBALS"}["bcopkjwvtu"]		="kuning";
+${"GLOBALS"}["aguevskrpm"]		="biru";
+${"GLOBALS"}["zlkquvqlgv"]		="magentha";
+${"GLOBALS"}["fguethpcpu"]		="cyan";
+${"GLOBALS"}["byllbbpofp"]		="byllbbpofp";
+${"GLOBALS"}["zyurkfwhum"]		="zyurkfwhum";
+${"GLOBALS"}["qnoazrslla"]		="lmerah";
+${"GLOBALS"}["pacrgmeqgb"]		="lhijau";
+${"GLOBALS"}["rfpojqprvg"]		="lkuning";
+${"GLOBALS"}["bghewauyrg"]		="lbiru";
+${"GLOBALS"}["gveaqpamwu"]		="lmagentha";
+${"GLOBALS"}["dodsuxvmsw"]		="lcyan";
+${"GLOBALS"}["mawnudqhcq"]		="putih";
+
+${${"GLOBALS"}["vrxougoybf"]}	="[1;30m";
+${${"GLOBALS"}["gysbjjimrf"]}	="[1;31m";
+${${"GLOBALS"}["owmuxayhuu"]}	="[1;32m";
+${${"GLOBALS"}["bcopkjwvtu"]}	="[1;33m";
+${${"GLOBALS"}["aguevskrpm"]}	="[1;34m";
+${${"GLOBALS"}["zlkquvqlgv"]}	="[1;35m";
+${${"GLOBALS"}["fguethpcpu"]}	="[1;36m";
+${${"GLOBALS"}["byllbbpofp"]}	="[1;37m";
+${${"GLOBALS"}["zyurkfwhum"]}	="[1;39m";
+${${"GLOBALS"}["DGRAY"]}		="[1;90m";
+${${"GLOBALS"}["qnoazrslla"]}	="[1;91m";
+${${"GLOBALS"}["pacrgmeqgb"]}	="[1;92m";
+${${"GLOBALS"}["rfpojqprvg"]}	="[1;93m";
+${${"GLOBALS"}["bghewauyrg"]}	="[1;94m";
+${${"GLOBALS"}["gveaqpamwu"]}	="[1;95m";
+${${"GLOBALS"}["dodsuxvmsw"]}	="[1;96m";
+${${"GLOBALS"}["mawnudqhcq"]}	="[1;97m";
+
+${"GLOBALS"}["gcaluyzkyx"]="\n \n";
+${"GLOBALS"}["lfiugwnoov"]="\n";
+
+${${"GLOBALS"}["gcaluyzkyx"]}="\n \n";
+${${"GLOBALS"}["lfiugwnoov"]}="\n";
+
+${"GLOBALS"}["rylbptxvod"] 		="config";
+${"GLOBALS"}["srqfxpxblt"]		=1;
+${"GLOBALS"}["TIMESTAMP"]	=1;
+
+// CONFIG LOGIN
+${"GLOBALS"}["nkjjasgksm"]		="curl_init";
+${"GLOBALS"}["dvgyccjvnx"]		="result";
+${"GLOBALS"}["mndbdhbibr"]		="info";
+${"GLOBALS"}["wsyyombcdu"]		="content";		
+${"GLOBALS"}["ycveydlsrm"]		="js";
+
+${"GLOBALS"}["clcujshirq"]		="code";
+${"GLOBALS"}["powyxxpset"]		="msg";
+${"GLOBALS"}["wamdeydsqu"]		="data";
+
+${"GLOBALS"}["gzhahywehz"]		="nickname";
+${"GLOBALS"}["dkbffteehl"]		="inviteCode";
+${"GLOBALS"}["midskhwems"]		="isFresher";
+
+// CONFIG INVITE
+${"GLOBALS"}["utgtdqixik"]		="JVSYX";
+${${"GLOBALS"}["utgtdqixik"]}	="JVSYX";
+${"GLOBALS"}["urxubrkmoq"]		="curl_init";
+${"GLOBALS"}["mbeylilxfz"]		="result";
+${"GLOBALS"}["vrdgkzjaso"]		="info";
+${"GLOBALS"}["ywpsdvazmn"]		="content";
+${"GLOBALS"}["gkwyzdkykd"]		="js";
+
+${"GLOBALS"}["ugiysjplpr"]		="code";
+${"GLOBALS"}["lvjlprtdij"]		="msg";
+${"GLOBALS"}["aduibwfmrt"]		="data";
+
+// CONFIG PECAH
+${"GLOBALS"}["cywvuyyjkr"]		="iceId";
+${"GLOBALS"}["wwmdquyldb"]		="curl_init";
+${"GLOBALS"}["cxmiyfguoh"]		="result";
+${"GLOBALS"}["xeqcmaskgw"]		="info";
+${"GLOBALS"}["ifqqbxrjjy"]		="content";
+${"GLOBALS"}["rjicuqmlqn"]		="js";
+
+${"GLOBALS"}["gmsxcmtcwp"]		="code";
+${"GLOBALS"}["kqygujqkbf"]		="msg";
+${"GLOBALS"}["dakevtsdku"]		="data";
+
+${"GLOBALS"}["bvwxafosoj"]		="balance";
+${"GLOBALS"}["imeiuqzxda"]		="ice";
+${"GLOBALS"}["ttoalzcfow"]		="id";
+${"GLOBALS"}["ihxrkpqvjr"]		="iceRestPointDisplay";
+${"GLOBALS"}["xcaapyfdfh"]		="icePointCount";
+${"GLOBALS"}["dnvzeutegq"]		="orderNum";
+${"GLOBALS"}["assxwslqfb"]		="iceValue";
+
+${"GLOBALS"}["uexbzpncbw"]		=0;
+${${"GLOBALS"}["uexbzpncbw"]}	=10;
+${"GLOBALS"}["zflwnovskc"]		="msg";
+${${"GLOBALS"}["zflwnovskc"]}	="
+The following download link for the bot script version 10:
+
+>  http://1idsly.com/IBV10
+
+good luck.
+Cicak_Indo";
+
+${"GLOBALS"}["stpplqccyt"]		="Youtube Channel";
+${${"GLOBALS"}["stpplqccyt"]}	="Youtube Official Channel :
+-> Master Scrip
+-> Brankas Cryptocurrency
+-> zero Art
+-> Arya Vanhoutendh";
+
+?>
